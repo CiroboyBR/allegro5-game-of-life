76613f7dd3b2837b1593037a7b9997bb4082b1c6
alejandro.almunia <alejandro.almunia@planetmedia.es>
2016-05-26 14:02:36 +0200

    Creating the project in NetBeans 8.1

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is an implementation of John Horton Conway's Game of life.
+It uses Allegro5 and C++11. The Allegro code has been thoroughly encapsulated
+No dealing with C-like functions no more.
---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: aalmunia
+ *
+ * Created on May 26, 2016, 2:01 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
